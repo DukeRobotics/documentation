619d1e3a4931b9e2535ff0165e2cc99096cc6166
Samuel Rabinowitz <13713786+rabinowitz@users.noreply.github.com>
2020-09-08 00:08:56 -0400

    Apply suggestions from code review
    Co-authored-by: Muthu Arivoli <ma381@duke.edu>

--- ros-tutorials/catkin_ws/build.sh
+++ ros-tutorials/catkin_ws/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # A convenience script used to build our code.
 
-set -e
+set -ex
 
 source /opt/ros/melodic/setup.bash
 
 catkin build
 source devel/setup.bash
-cd ../..
-
+set +x
 echo ""
 echo "Now, please run:"
 echo "source devel/setup.bash"
