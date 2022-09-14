6e2e4f5f95ead67ca1bf0a56ae858adc27084411
Drew Council <45700916+AndrewCouncil@users.noreply.github.com>
2022-09-14 17:10:20 -0400

    Installation updates (#23)
    * script for installs using winget
    
    * updated with winget script instructions and fixes

---
+++ ros-tutorials/winget-ros-configuration.ps1
@@ -0,0 +1,17 @@
+# This script will install the following applications:
+# - Docker Desktop
+# - Git
+# - GitHub Desktop
+# - Windows Terminal
+# - Slack
+# - Python3
+# The script also gives the option to install VS Code.
+# winget MUST BE INSTALLED BEFORE RUNNING THIS SCRIPT
+
+winget install -e --id Docker.DockerDesktop;winget install -e --id Git.Git;winget install -e --id GitHub.GitHubDesktop;winget install -e --id Microsoft.WindowsTerminal;winget install -e --id SlackTechnologies.Slack;winget install -e --id Python.Python.3
+# Ask user if they want to install the optional packages
+$response = Read-Host -Prompt "Do you want to install VS Code? (y/N)"
+# If $respose is y, install the optional packages
+if ($response -eq 'y'){
+    winget install -e --id Microsoft.VisualStudioCode
+}
