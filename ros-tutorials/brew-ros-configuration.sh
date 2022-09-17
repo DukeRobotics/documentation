3a3d1675d75aac4c5f914c6cc3d7cc0cab9e29fb
AndrewCouncil <andrew.p.council@gmail.com>
2022-09-17 14:13:50 -0400

    mac install script untested

---
+++ ros-tutorials/brew-ros-configuration.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# This script will install the following applications:
+# - brew
+# - Docker Desktop
+# - Git
+# - GitHub Desktop
+# - kitty
+# - Slack
+# - Python3
+# The script also gives the option to install VS Code.
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Docker Desktop
+brew install --cask docker
+
+# Install Git
+brew install git
+
+# Install GitHub Desktop
+brew install --cask github
+
+# Install kitty
+brew install --cask kitty
+
+# Install Slack
+brew install --cask slack
+
+# Install Python3
+brew install python3
+
+# Install VS Code
+echo "Do you want to install VS Code? (y/n)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    brew install --cask visual-studio-code
+else
+    echo "VS Code not installed"
+fi
