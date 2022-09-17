125b45827e71eb6accd38c6d7569b0a9e33631d0
AndrewCouncil <andrew.p.council@gmail.com>
2022-09-17 14:13:56 -0400

    more readable

--- ros-tutorials/winget-ros-configuration.ps1
+++ ros-tutorials/winget-ros-configuration.ps1
@@ -8,7 +8,13 @@
 # The script also gives the option to install VS Code.
 # winget MUST BE INSTALLED BEFORE RUNNING THIS SCRIPT
 
-winget install -e --id Docker.DockerDesktop;winget install -e --id Git.Git;winget install -e --id GitHub.GitHubDesktop;winget install -e --id Microsoft.WindowsTerminal;winget install -e --id SlackTechnologies.Slack;winget install -e --id Python.Python.3
+
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Git.Git
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id SlackTechnologies.Slack
+winget install -e --id Python.Python.3
 # Ask user if they want to install the optional packages
 $response = Read-Host -Prompt "Do you want to install VS Code? (y/N)"
 # If $respose is y, install the optional packages
