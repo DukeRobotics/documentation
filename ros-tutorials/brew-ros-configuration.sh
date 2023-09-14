6bf82f2dcc5a8ff2b66a8f8b3f2c91f79a3c1278
AndrewCouncil <andrew.p.council@gmail.com>
2023-09-14 17:27:47 -0400

    nonfunctional disk space check

--- ros-tutorials/brew-ros-configuration.sh
+++ ros-tutorials/brew-ros-configuration.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# if available disk space less than 25GB, exit and print error message
-if [ $(df -H | grep -m1 -o '[0-9]\+G' | tr -d G) -lt 25 ]; then
-    echo "Error: Available disk space less than 25GB"
-    exit 1
-fi
-
 # if homebrew is not installed, install homebrew
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed, installing homebrew..."
